4de767c851a119a15a5d267bef2b0e374d878451
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-09-26 15:08:37 -0300

    Geostate

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomePage extends StatefulWidget {
   //const HomePage({super.key});
@@ -8,25 +9,60 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+var state = GeoState.square();
+
 class _HomePageState extends State<HomePage> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-    const squareSize = 300.0;
-    final color = Colors.blue;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Masterclass 5'),
       ),
       body: Center(
         child: AnimatedContainer(
-          duration: Duration(seconds: 2),
-          color: color,
-          height: squareSize,
-          width: squareSize,
+          duration: const Duration(seconds: 4),
+          decoration: BoxDecoration(
+            color: state.color,
+            borderRadius: BorderRadius.circular(state.radius),
+          ),
+          height: state.squareSize,
+          width: state.squareSize,
         ),
       ),
+    );
+  }
+}
+
+enum Geotype { square, cricle }
+
+class GeoState {
+  final double squareSize;
+  final Color color;
+  final double radius;
+  final Geotype type;
+
+  GeoState({
+    required this.squareSize,
+    required this.color,
+    required this.radius,
+    required this.type,
+  });
+
+  factory GeoState.square() {
+    return GeoState(
+      squareSize: 200,
+      color: Colors.red,
+      radius: 200,
+      type: Geotype.square,
+    );
+  }
+  factory GeoState.circle() {
+    return GeoState(
+      squareSize: 200,
+      color: Colors.blue,
+      radius: 200,
+      type: Geotype.cricle,
     );
   }
 }
